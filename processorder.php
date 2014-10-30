ba1c4ae61f7aa655bb664ab53b74f81046d0c95f
shenliang <shenliang@tose.com.cn>
2014-10-30 16:07:49 +0800

    simple php to process order echo something

---
+++ processorder.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>order result</title>
+</head>
+<body>
+	<h1>Bob's auto Parts</h1>
+	<h2>Order Results</h2>
+	<?php
+	echo "<p>Oerder processed at ";
+	echo date ( 'H:i, jS F Y' );
+	echo "</p>";
+	echo "<p>Oerder processed at " . date ( 'H:i, jS F Y' ) . "</p>";
+	?>
+</body>
+</html>
