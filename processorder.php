c6337248b1044c61884802beda38f334d41487ba
shenliang <shenliang@tose.com.cn>
2014-10-30 16:59:32 +0800

    取得post数据的3种方式，其中两种无法使用

--- processorder.php
+++ processorder.php
@@ -10,6 +10,13 @@
 	echo date ( 'H:i, jS F Y' );
 	echo "</p>";
 	echo "<p>Oerder processed at " . date ( 'H:i, jS F Y' ) . "</p>";
+	
+	// 3 ways to get post
+	echo "<p>";
+	echo "short style:" . $tireqty;//not work
+	echo " medium style:" . $_POST ['tireqty'];
+	echo " long style:" . $HTTP_POST_VARS ['tireqty'];//not work
+	echo "</p>";
 	?>
 </body>
 </html>
