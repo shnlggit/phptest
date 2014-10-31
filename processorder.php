904e0e86564e9c8f4d305e12393f5db9556511d4
shenliang <shenliang@tose.com.cn>
2014-10-31 09:22:02 +0800

    calculate

--- processorder.php
+++ processorder.php
@@ -13,10 +13,29 @@
 	
 	// 3 ways to get post
 	echo "<p>";
-	echo "short style:" . $tireqty;//not work
+	echo "short style:" . $tireqty; // not work
 	echo " medium style:" . $_POST ['tireqty'];
-	echo " long style:" . $HTTP_POST_VARS ['tireqty'];//not work
+	echo " long style:" . $HTTP_POST_VARS ['tireqty']; // not work
 	echo "</p>";
+	
+	$tireqty = $_POST ['tireqty'];
+	$oilqty = $_POST ['oilqty'];
+	$sparkqty = $_POST ['sparkqty'];
+	$totalqty = 0;
+	$totalqty = $tireqty + $oilqty + $sparkqty;
+	echo "Items ordered $totalqty<br/>";
+	
+	define ( 'TIREPRICE', 100 );
+	define ( 'OILPRICE', 10 );
+	define ( 'SPARKPRICE', 4 );
+	
+	$totalamount = 0.0;
+	$totalamount = $tireqty * TIREPRICE + $oilqty * OILPRICE + $sparkqty + SPARKPRICE;
+	echo 'Subtotal: $' . number_format ( $totalamount, 2 ) . '<br/>';
+	
+	$taxrate = 0.10;
+	$totalamount = $totalamount * (1 + $taxrate);
+	echo 'Total include tax: $' . number_format ( $totalamount, 2 ) . '<br/>';
 	?>
 </body>
 </html>
